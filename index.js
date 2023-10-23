2c8b4905e73b6e51e6815cf289dd57f442038f01
sohkX1EG4 <sohk@live.com>
2023-10-23 18:19:56 +0800

    publish to npm

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 import isotopes from './allIsotopes.json';
 
-export default function(element) {
+function getInfoByElement(element) {
   const elementInfo = isotopes.find(item => item.element === element);
 
   if (!elementInfo) return {};
 
   return elementInfo;
+}
+
+function getInfoByAtomicNumber(atomicNumber) {
+  const elementInfo = isotopes.find(item => item.atomic_number === atomicNumber);
+
+  if (!elementInfo) return {};
+
+  return elementInfo;
+}
+
+export {
+  isotopes,
+  getInfoByElement,
+  getInfoByAtomicNumber
 }
